3cacb733f028699baf86bf7b99cfa753d1b50e29
Rusty Bentley <mycoding.alert@gmail.com>
2015-10-19 22:34:13 -0500

    valid-solution? passing

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -84,7 +84,7 @@
   (every? eq-all-values? (blocks board)))
 
 (defn valid-solution? [board]
-  nil)
+  (and (valid-rows? board) (valid-cols? board) (valid-blocks? board)))
 
 (defn set-value-at [board coord new-value]
   nil)
